8da1149ea4d9a4781fee5c0931d5a28178c8a73d
vishaljadhav7 <vishalj.7661@gmail.com>
2024-11-16 15:42:58 +0530

    used postman (http client) to test the APIs && used nodemon to avoid restarting the server after making changes

--- src/index.js
+++ src/index.js
@@ -1,9 +1,31 @@
 const express = require('express')
 const app = express()
 
-app.use((req, res) => {
-    res.send("Hello world")
+app.use("/test", (req,res) => {
+    res.send(" route /test")
 })
+app.use("/hey" , (req,res) => {
+    res.send(" route /hey")
+})
+
+app.use('/route1',(req,res) => {
+    res.send(" route /route1")
+} )
+
+
+app.use('/route2',(req,res) => {
+    res.send(" route /route2")
+} )
+
+
+
+app.use("/", (req,res) => {
+    res.send("main route /")
+})
+
+
+
+
 
 app.listen(3000, ()=> {
   console.log("server listening on port 3000")   
