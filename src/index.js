93858fe43b07c9a85600eea40b71280548414adc
vishaljadhav7 <vishalj.7661@gmail.com>
2024-11-18 14:42:53 +0530

    wrote a patch /user API to update the allowed user profile details

--- src/index.js
+++ src/index.js
@@ -9,7 +9,6 @@ app.use(express.json())
 app.post('/signup', async (req, res) => {
     try{
         const newUser = new user({...req.body})
-        console.log(newUser)
         await newUser.save()
         res.send("user added succesfully") 
     }
@@ -22,14 +21,55 @@ app.post('/signup', async (req, res) => {
 
 
 app.get('/feed', async (req, res) => {
+
+  try {
     const allUsers = await  user.find({})  
-  
+     if(allUsers.length === 0){
+        throw new Error("users not found")
+     } 
     res.json(allUsers)
+  } catch (error) {
+    res.status(404).json({
+        message : error.message
+    })
+  }
 })
+
+app.patch('/user/:userId', async (req, res) => {
+  const userId = req.params?.userId;
+  const userData = req.body;
+
+
+  try {
+    const ALLOWED_UPDATES = ["photoURL", "about", "gender", "age" , "skills", "lastName"]
+
+    const upatedAllowed = Object.keys(userData).every(key => ALLOWED_UPDATES.includes(key))
+
+    if(!upatedAllowed){
+        throw new Error('update not allowed');
+    }
+
+    if(userData?.skills?.length > 10){
+        throw new Error('skill more than 10 not allowed');
+    }
+
+    const updatedUser = await user.findByIdAndUpdate({ _id : userId}, userData , {returnDocument : 'after', runValidators : 'true'} )
+     res.status(200).json({
+        message : "update success",
+        data : updatedUser
+     })
+
+  } catch (error) {
+    res.status(400).json({
+        message : "update not success because " + error.message
+     })
+  }
+})
+
+
 
 connectDB().then(()=>{
     console.log("DB connection established")
-
     app.listen(process.env.PORT , ()=> {
       console.log("server listening on port 3000 ")   
     }) 
--- src/models/user.model.js
+++ src/models/user.model.js
@@ -43,6 +43,10 @@ const userSchema = new mongoose.Schema({
     about : {
         type : String,
         default : "I am a developer"
+    },
+    photoURL : {
+        type : String,
+        default : "https://cdn.pixabay.com/photo/2015/10/05/22/37/blank-profile-picture-973460_1280.png"
     }
 }, {timestamps : true});
 
