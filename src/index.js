3e428b5591085b07ce1a7c3843c82ff6842d5280
vishaljadhav7 <vishalj.7661@gmail.com>
2024-12-24 15:53:56 +0530

    done with setting up   HTTP server with integrated Express and Socket.IO

--- src/index.js
+++ src/index.js
@@ -2,10 +2,13 @@ require('dotenv').config()
 
 const http = require('http');
 const app = require('./app')
-const PORT = Number(process.env.PORT || 3001)
+const PORT = Number(process.env.PORT || 3001);
+const initializeSocket = require('./socket')
 const connectDB = require('./config/database')
  
 const httpServer = http.createServer(app);
+
+initializeSocket(httpServer);
 
 connectDB().then(()=>{
     console.log("DB connection established")
