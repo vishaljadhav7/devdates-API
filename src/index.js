6426362731058aaab0177180c38423d00ebe12a7
vishaljadhav7 <vishalj.7661@gmail.com>
2024-12-24 15:50:58 +0530

    done with setting up HTTP server with integrated Express and Socket.IO

--- src/index.js
+++ src/index.js
@@ -1,12 +1,15 @@
 require('dotenv').config()
 
+const http = require('http');
 const app = require('./app')
 const PORT = Number(process.env.PORT || 3001)
 const connectDB = require('./config/database')
  
+const httpServer = http.createServer(app);
+
 connectDB().then(()=>{
     console.log("DB connection established")
-    app.listen(PORT , ()=> {
+    httpServer.listen(PORT , ()=> {
       console.log("server listening on port 3000 ")   
     }) 
 }) 
---
+++ src/socket.js
@@ -0,0 +1,33 @@
+const socketIo = require('socket.io');
+let io;
+
+function initializeSocket(server) {
+    io = socketIo(server, {
+        cors: {
+            origin: '*',
+            methods: [ 'GET', 'POST' ]
+        }
+    });
+
+    io.on('connection', (socket) => {
+        console.log(`Client connected: ${socket.id}`);
+
+
+        socket.on('disconnect', () => {
+            console.log(`Client disconnected: ${socket.id}`);
+        });
+    });
+}
+
+const sendMessageToSocketId = (socketId, messageObject) => {
+
+console.log(messageObject);
+
+    if (io) {
+        io.to(socketId).emit(messageObject.event, messageObject.data);
+    } else {
+        console.log('Socket.io not initialized.');
+    }
+}
+
+module.exports = { initializeSocket, sendMessageToSocketId };
